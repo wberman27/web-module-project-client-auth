df5cebdaa7b7a3f37a89bb002a82957ef5fcdd20
Will B <wberman27@gmail.com>
2021-03-23 18:21:10 -0400

    added minimal styling

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Login from './components/Login'
+
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello </h1>
+      <h1> Imaginary Friends </h1>
+      <Login />
     </div>
   );
 }
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../App.css'
 
 class Login extends React.Component {
     state = {
@@ -7,4 +8,28 @@ class Login extends React.Component {
             password: ''
         }
     }
+
+
+    render(){
+        return(
+            <div className= 'form-wrapper'>
+                <form>
+                    <label>Account
+                    <input type='text' name='username' onChange = {()=>{}} value={this.state.credentials.username} placeholder='enter username'/>
+                    </label>
+                    
+                    <label>Password
+                    <input type='text' name='password' onChange = {()=>{}} value={this.state.credentials.password} placeholder='enter password'/>
+                    </label>
+
+                </form>
+
+                <button onSubmit = {()=>{}}>Log in</button>
+
+            </div>
+
+        );
+    }
 }
+
+export default Login
