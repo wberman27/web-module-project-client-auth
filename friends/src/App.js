e8ddcb0929c98b01911fcc41c8a0212cb0df1c09
Will B <wberman27@gmail.com>
2021-03-23 18:37:54 -0400

    fixed styling issues

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,29 @@
+import React from 'react'
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
+
 import './App.css';
 import Login from './components/Login'
 
 
 function App() {
   return (
-    <div className="App">
-      <h1> Imaginary Friends </h1>
-      <Login />
-    </div>
+    <Router>
+      <div className='Nav'>
+        <h1> Imaginary Friends </h1>
+        <ul>
+            <li>
+              <Link to='login'>Login</Link>
+            </li>
+
+        </ul>
+      </div>
+      
+      <div className="App">
+        
+        <Login />
+        
+      </div>
+    </Router>
   );
 }
 
